e9edee6391916c344852e13135541cee555dda5f
lujanfernaud <lujanfernaud@gmail.com>
2017-05-11 14:35:59 +0100

    Start new design

--- knights_travails.rb
+++ knights_travails.rb
@@ -1,56 +1,55 @@
 require "pry"
 
-class Board
-  attr_accessor :matrix
+class Graph
+  attr_accessor :root
 
-  def initialize
-    # @matrix = Array.new(8) { Array.new(8) { 0 } }
-    @matrix = Array.new(8) { [1, 2, 3, 4, 5, 6, 7, 8] }
-    create_graph
-  end
-
-  def create_graph
-    @matrix.each do |x|
-      x.each do |y|
-        vertex = Vertex.new(y)
-        x[x.index(y)] = vertex
-      end
-    end
-
-    @matrix.each do |x|
-      x.each do |y|
-        vertex = y
-        add_edges(vertex, @matrix.index(x), x.index(y))
-      end
-    end
-  end
-
-  def add_edges(vertex, x, y)
-    vertex.edges << @matrix[x + 1][y] if x >= 0 && x <= 6
-    vertex.edges << @matrix[x - 1][y] if x >= 1 && x <= 7
-    vertex.edges << @matrix[x][y + 1] if y >= 0 && y <= 6
-    vertex.edges << @matrix[x][y - 1] if y >= 1 && y <= 7
+  def initialize(data)
+    @root = Vertex.new(data)
   end
 end
 
 class Vertex
-  attr_accessor :value, :edges
+  attr_accessor :data, :parent, :edges
 
-  def initialize(value = nil, edges = [])
-    @value = value
-    @edges = edges
+  def initialize(data = nil)
+    @data   = data
+    @parent = nil
+    @edges  = []
+  end
+
+  def insert(data)
+    vertex = Vertex.new(data)
+    vertex.parent = self
+    @edges << vertex
+  end
+end
+
+class Board
+  attr_accessor :board
+
+  def initialize
+    create_board
+  end
+
+  def create_board
+    @board = []
+    8.times { |x| 8.times { |y| board << [x, y] } }
   end
 end
 
 class Knight
-  attr_accessor :board
+  attr_accessor :board, :position
 
-  def initialize
-    @board = Board.new
+  ALLOWED_MOVES = [[-2, 1], [-1, 2], [2, 1],  [1, 2],
+                   [-1, 2], [-2, 1], [-1, 2], [-2, -1]].freeze
+
+  def initialize(position = [0, 0])
+    @board    = Board.new
+    @position = position
   end
 
   def move(start, finish)
   end
 end
 
-board = Board.new
+knight = Knight.new
